95df83abac16bfa9d6f0304b834c4820f01bd5d4
poinalex01 <poinalex01@gmail.com>
2025-01-26 18:04:23 +0100

    Initial commit

---
+++ src/main/java/net/poinalex01/testerino/TesterinoClient.java
@@ -0,0 +1,10 @@
+package net.poinalex01.testerino;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TesterinoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/poinalex01/testerino/TesterinoDataGenerator.java
@@ -0,0 +1,11 @@
+package net.poinalex01.testerino;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TesterinoDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
