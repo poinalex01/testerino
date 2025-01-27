86109ab354910babd8e0dbe714ecaeab4474a2cf
poinalex01 <poinalex01@gmail.com>
2025-01-27 02:43:13 +0100

    added ruby

--- src/main/java/net/poinalex01/testerino/Testerino.java
+++ src/main/java/net/poinalex01/testerino/Testerino.java
@@ -1,7 +1,7 @@
 package net.poinalex01.testerino;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.poinalex01.testerino.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,12 +13,12 @@ public class Testerino implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/poinalex01/testerino/item/ModItems.java
@@ -0,0 +1,31 @@
+package net.poinalex01.testerino.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.poinalex01.testerino.Testerino;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static final Item RUBY = registerItem("ruby", Item::new, new Item.Settings());
+
+
+    private static Item registerItem(String name, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Testerino.MOD_ID, name));
+        return Items.register(registryKey, factory, settings);
+    }
+    private static void customIngredients(FabricItemGroupEntries entries) {
+        entries.add(RUBY);
+    }
+    public static void registerModItems() {
+        Testerino.LOGGER.info("Registering Mod Items for " + Testerino.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::customIngredients);
+    }
+}
